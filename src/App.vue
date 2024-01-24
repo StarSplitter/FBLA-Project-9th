9981338cb56d03ddf30277f2b42d13eb96a0a556
Tianluo Zhu <leozhu0821@gmail.com>
2024-01-23 22:00:52 -0700

    getting rid of templates

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,7 @@
 
     const toast = useToast();
 
-    const courses = ref([
-        {id: 1, text: "Algebra 2", weighted: false, grade: 4},
-        {id: 2, text: "Honors 9th Comp", weighted: false, grade: 4},
-        {id: 3, text: "AP Human Geography", weighted: true, grade: 4},
-        {id: 4, text: "Honors Biology", weighted: false, grade: 3},
-        {id: 5, text: "Honors Spanish I", weighted: false, grade: 3}
-    ]);
+    const courses = ref([]);
     const unweightedGPA = computed(() => {
         return courses.value.reduce((a, course) => {
             return a + course.grade;
